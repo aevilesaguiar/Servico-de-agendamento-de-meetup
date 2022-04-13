01e76eaa6d0f73b988aa343a55fe15692e88405a
aevilesaguiar <aeviles@hotmail.com>
2022-04-13 09:21:03 -0300

    configuração swaager

--- src/main/java/com/womakerscode/microsservicemeetup/config/SwaggerConfig.java
+++ src/main/java/com/womakerscode/microsservicemeetup/config/SwaggerConfig.java
@@ -1,15 +1,20 @@
 package com.womakerscode.microsservicemeetup.config;
 
+
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
+
 
 @Configuration
 @EnableSwagger2
@@ -19,8 +24,8 @@ public class SwaggerConfig {
     public Docket docket() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis( RequestHandlerSelectors.basePackage("com.bootcamp.microservicemeetup.controller.resource") )
-                .paths(PathSelectors.any())
+                .apis((Predicate<RequestHandler>) basePackage("com.bootcamp.microservicemeetup.controller.resource"))
+                .paths((Predicate<String>) PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
@@ -35,9 +40,8 @@ public class SwaggerConfig {
     }
 
     private Contact contact() {
-        return new Contact("Womakerscode Aeviles Aguiar",
-                "http://womakerscode.org",
-                "aeviles@hotmail.com");
+        return new Contact("Aeviles Aguiar",
+                "https://www.linkedin.com/in/aeviles-aguiar-silva/",
+                "aeviles@hotmaill.com");
     }
-
 }
